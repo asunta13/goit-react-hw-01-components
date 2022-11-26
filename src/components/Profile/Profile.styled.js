0f65e521c2e8075ae43d9c618f3eab4771a8e949
Anastasiya Pekarik <anastasiya.pekarik@gmail.com>
2022-11-26 17:41:09 +0200

    styled added

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,60 +1,67 @@
-.profile {
+import styled from 'styled-components';
+
+export const ProfileWrapper = styled.div`
+  margin: auto;
   padding: 50px;
   width: 300px;
   background-color: lightgray;
-}
+`;
 
-.description {
+export const Description = styled.div`
   padding: 25px;
-  background-color: white;
+  background: white;
   display: flex;
   flex-direction: column;
-}
+`;
 
-.avatar {
+export const Avatar = styled.img`
   display: block;
   margin: auto;
   width: 100px;
   border-radius: 50px;
   padding: 10px;
-}
+`;
 
-.name {
+export const Name = styled.p`
   font-size: 24px;
   margin: auto;
   font-weight: bold;
-}
+`;
 
-.tag {
+export const Tag = styled.p`
   color: grey;
   margin: auto;
   padding: 10px;
-}
-.location {
+`;
+
+export const Location = styled.p`
   color: grey;
   margin: auto;
   padding: 10px;
-}
-.stats {
+`;
+
+export const Stats = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
   display: flex;
-}
-.label {
-  margin: auto;
-  padding-bottom: 10px;
-}
-.quantity {
-  margin: auto;
-  font-size: 18px;
-  font-style: bold;
-}
-.item {
+`;
+export const Item = styled.li`
   width: 100px;
   padding: 10px;
   display: flex;
   flex-direction: column;
   background-color: azure;
   border: 1px solid lightgrey;
-}
+`;
+
+export const Label = styled.span`
+  margin: auto;
+  padding-bottom: 10px;
+`;
+
+export const Quantity = styled.span`
+  margin: auto;
+  font-size: 18px;
+  font-style: bold;
+`;
